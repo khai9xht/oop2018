968b131c9facb52523429ea7bf68a6af8cab42b3
khai9xht <khaihoang9x@gmail.com>
2018-09-12 13:51:07 +0700

    bài tập week1

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,38 +1,48 @@
 package week1;
-
 public class Student {
-
-    // TODO: khai báo các thuộc tính cho Student
-
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
-    Student() {
-        // TODO:
+    private String name,email,group;
+    private int id;
+    //contructor có tham số
+    public Student(String Name, String Group, String Email, int Id){
+        name = Name;
+        group = Group;
+        email = Email;
+        id = Id;
     }
-
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
-    Student(String n, String sid, String em) {
-        // TODO:
+    //contructor khong có tham số
+    public Student() {
+        name = "Student";
+        group = "INT 2204 4";
+        email = "khaihoang9x@gmail.com";
+        id = 0;
     }
-
-    /**
-     * Constructor 3
-     * @param s
-     */
-    Student(Student s) {
-        // TODO:
+    //contructor sao chép
+    public Student(Student s){
+        this.name = s.name;
+        this.group = s.group;
+        this.email = s.email;
+        this.id = s.id;
     }
-
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+    public String getName(){
+        return name;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public String getInfo() {
+        return ("Your name: " + name + '\n' + "Your group: " + group
+                + '\n' + "Your id: " + id + '\n' + "Your email: " + email);
+    }
+    public void setAll(Student s1) {
+        this.name = s1.name;
+        this.id = s1.id;
+        this.email = s1.email;
+        this.group = s1.group;
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,73 @@
 package week1;
 
 public class StudentManagement {
-
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
-    public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
-    }
-
-    void studentsByGroup() {
-        // TODO:
-    }
-
-    void removeStudent(String id) {
-        // TODO:
-    }
-
     public static void main(String[] args) {
-        // TODO:
+        Student K62_uet = new Student("Nguyen Viet Hoang", "INT 2204 4",
+                "khaihoang9x@gmail.com", 17020771);
+        //  Student K62_uet = new Student();   //contructor không có tham so
+        Student a = new Student("Vu Viet Hoang", "INT 2204 4",
+                "vuviethoang@gmail.com", 17020712);
+        Student b = new Student("Nguyen Phuong Hieu","INT2204 11",
+                "hieuadh@gmail.com",17020672);
+
+        System.out.println(K62_uet.getInfo());
+
+        if (sameGroup(a,K62_uet)) System.out.println(a.getName()+" and " + K62_uet.getName() + " are same class");
+        else System.out.println(a.getName() + " and " + K62_uet.getName() + " are different class");
+        if (sameGroup(b,K62_uet)) System.out.println(b.getName()+" and " + K62_uet.getName() + " are same class");
+        else System.out.println(b.getName() + " and " + K62_uet.getName() + " are different class");
+
+        Student[] Array = new Student[100];
+        Array[0] = new Student("Nguyen Viet Hoang", "INT 2204 4",
+                "khaihoang9x@gmail.com", 17020771);
+        Array[1] = new Student("Vu Viet Hoang", "INT 2204 11",
+                "vuviethoang@gmail.com", 17020712);
+        Array[2] = new Student("Nguyen Phuong Hieu","INT 2204 1",
+                "hieuadh@gmail.com",17020642);
+        Array[3] = new Student("Nguyen Phuong Huy","INT 2204 11",
+                "hieuadh@gmail.com",17020622);
+        Array[4] = new Student("Nguyen huy Ha","INT 2204 4",
+                "hieuadh@gmail.com",17020692);
+        Array[5] = new Student("Nguyen thi Hoa","INT 2204 11",
+                "hieuadh@gmail.com",17020672);
+        removeStudent(Array, 6, 17020712);
+        studentsByGroup(Array, 5);
+
     }
+
+    public static boolean sameGroup(Student s1, Student s2) {
+        return (s1.getGroup()).equals(s2.getGroup());
+    }
+
+    public static void studentsByGroup(Student[] array, int n) {
+        boolean[] checkGroup = new boolean[n]; // kiểm tra lượt check
+        for(int i=0;i<n;i++) checkGroup[i]=false; //tất cả chưa check
+
+        for(int i=0;i<n;i++){
+            if(!checkGroup[i]){
+                System.out.println("Group " + (array[i].getGroup()) + ": ");
+                checkGroup[i] = true;
+                for(int j=i;j<n;j++) {
+                    if ((array[j].getGroup()).equals(array[i].getGroup())) {
+                        System.out.println('\t' + array[j].getName());
+                        checkGroup[j] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void removeStudent(Student[] array,int n, int id) {
+        for(int i=0;i<n;i++){
+            if(id==array[i].getId()) {
+                for(int j=i;j<n-1;j++) {
+                    array[j].setAll(array[j+1]);
+                }
+                break;
+            }
+        }
+    }
+
+
 }
+
