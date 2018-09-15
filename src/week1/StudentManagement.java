cffdfa0139df71af9e202872aa1c5971abb562c6
lab2 <khai9xht>
2018-09-15 16:59:11 +0700

    bài tập lab_2

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,8 +1,10 @@
 package week1;
 public class StudentManagement {
-    private static int n=6;
-    private static Student[] Array = new Student[100];
-
+    private Student[] Array = new Student[100];
+    private int num;
+    public void setNum(int n){
+        num = n;
+    }
     public static void main(String[] args) {
         Student K62_uet = new Student("Nguyen Viet Hoang", "INT 2204 4",
                 "khaihoang9x@gmail.com", 17020771);
@@ -13,46 +15,46 @@ public class StudentManagement {
                 "hieuadh@gmail.com",17020672);
 
         System.out.println(K62_uet.getInfo());
-
-        if (sameGroup(a,K62_uet)) System.out.println(a.getName()+" and " + K62_uet.getName() + " are same class");
+        StudentManagement SM = new StudentManagement();
+        SM.num = 6;
+        if (SM.sameGroup(a,K62_uet)) System.out.println(a.getName()+" and " + K62_uet.getName() + " are same class");
         else System.out.println(a.getName() + " and " + K62_uet.getName() + " are different class");
-        if (sameGroup(b,K62_uet)) System.out.println(b.getName()+" and " + K62_uet.getName() + " are same class");
+        if (SM.sameGroup(b,K62_uet)) System.out.println(b.getName()+" and " + K62_uet.getName() + " are same class");
         else System.out.println(b.getName() + " and " + K62_uet.getName() + " are different class");
-        Array[0] = new  Student("Vu Viet Hoang", "INT 2204 4",
-                "vuviethoang@gmail.com", 17020712);
-        Array[1] = new Student("Nguyen Phuong Hieu","INT2204 1",
-                "hieuadh@gmail.com",17020672);
-        Array[2] = new Student("Nguyen Huy","INT2204 11",
-                "hieuadh@gmail.com",17020971);
-        Array[3] = new Student("Nguyen Phuong Hoa","INT2204 8",
-                "hieuadh@gmail.com",17020672);
-        Array[4] = new Student("Nguyen thi ha","INT2204 1",
-                "hieuadh@gmail.com",17020712);
-        Array[5] = new Student("Nguyen thi hang","INT2204 11",
-                "hieuadh@gmail.com",17020712);
-        removeStudent(17020971);
-        studentsByGroup();
 
+        SM.Array[0] = new Student("Nguyen Viet Hoang", "INT 2204 4",
+                "khaihoang9x@gmail.com", 17020771);
+        SM.Array[1] = new Student("Vu Viet Hoang", "INT 2204 11",
+                "vuviethoang@gmail.com", 17020712);
+        SM.Array[2] = new Student("Nguyen Phuong Hieu","INT 2204 1",
+                "hieuadh@gmail.com",17020642);
+        SM.Array[3] = new Student("Nguyen Phuong Huy","INT 2204 11",
+                "hieuadh@gmail.com",17020622);
+        SM.Array[4] = new Student("Nguyen huy Ha","INT 2204 4",
+                "hieuadh@gmail.com",17020692);
+        SM.Array[5] = new Student("Nguyen thi Hoa","INT 2204 11",
+                "hieuadh@gmail.com",17020672);
+        SM.removeStudent(17020712);
+        SM.studentsByGroup();
 
     }
 
-
-
-    public static boolean sameGroup(Student s1, Student s2) {
+    public boolean sameGroup(Student s1, Student s2) {
         return (s1.getGroup()).equals(s2.getGroup());
     }
 
-    public static void studentsByGroup() {
+    public void studentsByGroup() {
+        int n = this.num;
         boolean[] checkGroup = new boolean[n]; // kiểm tra lượt check
         for(int i=0;i<n;i++) checkGroup[i]=false; //tất cả chưa check
 
         for(int i=0;i<n;i++){
             if(!checkGroup[i]){
-                System.out.println("Group " + (Array[i].getGroup()) + ": ");
+                System.out.println("Group " + (this.Array[i].getGroup()) + ": ");
                 checkGroup[i] = true;
                 for(int j=i;j<n;j++) {
-                    if ((Array[j].getGroup()).equals(Array[i].getGroup())) {
-                        System.out.println('\t' + Array[j].getName());
+                    if ((this.Array[j].getGroup()).equals(this.Array[i].getGroup())) {
+                        System.out.println('\t' + this.Array[j].getName());
                         checkGroup[j] = true;
                     }
                 }
@@ -60,14 +62,14 @@ public class StudentManagement {
         }
     }
 
-    public static void removeStudent( int id) {
+    public void removeStudent(int id) {
+        int n = this.num;
         for(int i=0;i<n;i++){
-            if(id==Array[i].getId()) {
+            if(id==this.Array[i].getId()) {
                 for(int j=i;j<n-1;j++) {
-                    Array[j].setAll(Array[j + 1]);
+                    this.Array[j].setAll(this.Array[j+1]);
                 }
-                Array[n-1] = null;
-                n--;
+                this.setNum(n-1);
                 break;
             }
         }
@@ -75,4 +77,5 @@ public class StudentManagement {
 
 
 }
+
 
